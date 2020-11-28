2e3b03b6bc420648aa0918af315355a486c34d81
Denys <denys.holovko@gmail.com>
2020-11-28 13:13:17 +0200

    secont lesson

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const numberOfFilms = +prompt("Скільки фільмів ви вже подивилися?", "");
 
 const personalMovieDB = {
@@ -8,12 +10,28 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt("Один з останніх переглянутих фільмів?", ""),
-      b = +prompt("На скільки балів його оціните?", ""),
-      c = prompt("Один з останніх переглянутих фільмів?", ""),
-      d = +prompt("На скільки балів його оціните?", "");
+for (let i = 0; i < 2; i++) {
+    const a = prompt("Один з останніх переглянутих фільмів?", ""),
+          b = +prompt("На скільки балів його оціните?", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--; 
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('Переглянуто мало фільмів');
+} else if (personalMovieDB.count >= 10 && personalMovieDB < 30) {
+    console.log('Ви класичний глядач');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Ви кіноман');
+} else {
+    console.log('Сталася помилка');
+}
 
 console.log(personalMovieDB);
+
